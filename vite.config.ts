92430eeb74ce58389dd89943da034dc39bef2514
Thebe <thebes@staycontactable.com>
2026-02-14 15:47:05 +0200

    Project setup & dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
@@ -6,7 +7,16 @@ import viteSvgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), eslintPlugin(), viteSvgr(), tsconfigPaths()],
+  plugins: [
+    TanStackRouterVite({
+      routesDirectory: './src/routes',
+      generatedRouteTree: './src/routeTree.gen.ts'
+    }),
+    react(),
+    eslintPlugin(),
+    viteSvgr(),
+    tsconfigPaths()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
